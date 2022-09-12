a39c38307a1f265c0b4c697d42af2f0e03556f79
Marlon Brenes <91922207+BrenesRM@users.noreply.github.com>
2022-09-12 14:39:11 -0600

    Actualizar Ps1
    Make a file with the collumns for a second run with the error.

--- UpdateGroupsAttributes.ps1
+++ UpdateGroupsAttributes.ps1
@@ -1,11 +1,10 @@
-#Allways do the process Verified Validated, if remediation is needed, then run this PS1.
-#VerifiedValidatedGruposUpdate.ps1
-
 # Import active directory module for running AD cmdlets
 Import-Module ActiveDirectory
   
 # Store the data from .csv in the $File variable
-$File = Import-Csv E:\Scripts\GruposUpdate.csv -Delimiter ","
+$File = Import-Csv E:\Scripts\ActualizarAttibutesGroups\XXXX.csv -Delimiter ","
+$ErrorPath = "E:\Scripts\ActualizarAttibutesGroups\ErrorXXXX.txt"
+"Name" + ";" +  "Description"  + ";" + "ManageBy" + ";" + "Info" + ";" + "error" + ";" +  "Fecha" | Set-Content $ErrorPath
 
 # Loop through each row containing user details in the CSV file
 foreach ($line in $File) {
@@ -13,30 +12,56 @@ foreach ($line in $File) {
     Try
         {
     # Try to add / Descriptio, ManagedBy, Co - dueno (active roles), info (Notas, info)
-        Set-ADGroup -Identity ($line.Name) -Description ($line.Description) -Passthru
+        
+        try 
+        {
+            Set-ADGroup -Identity ($line.Name) -Description ($line.Description) -Passthru
+        }
+        catch 
+        {
+           write-host "Description is in blank, is not posible update atributo description, ($line.Description)"
+           $Errortry = ($line.Name + ";" + ($line.Description) + ";" + ($line.Managedby) + ";" + ($line.Info) + ";" + $_ + ";Fecha: " + (Get-Date))
+           Add-Content ($ErrorPath) -value ($Errortry)
+
+        }
+        
         
         try
            {
            #Check if the user exist.
-           Set-ADGroup -Identity ($line.Name) -ManagedBy ($line.Managedby) -Passthru 
+           
+           if (($line.Managedby.Length) -gt 1) {
+                Set-ADGroup -Identity ($line.Name) -ManagedBy ($line.Managedby) -Passthru
+           }else {
+                $Errortry = ($line.Name + ";" + ($line.Description) + ";" + "Sin Manager" + ";" + ($line.Info) + ";" + "Falta Definir el Manager" + ";Fecha: " + (Get-Date))
+                Add-Content ($ErrorPath) -value ($Errortry)
+           }
            }
         catch
            {
            write-host "User is not in AD, is not posible update atributo Manageby, ($line.Managedby)"
-           $Errortry = ($line.Name + "," + $line.Managedby + "," + $_ + ",Fecha: " + (Get-Date))
-           Add-Content E:\Scripts\Error.txt -value ($Errortry)
+           $Errortry = ($line.Name + ";" + ($line.Description) + ";" + ($line.Managedby) + ";" + ($line.Info) + ";" + $_ + ";Fecha: " + (Get-Date))
+           Add-Content ($ErrorPath) -value ($Errortry)
            }
-        #Set-ADGroup -Identity ($line.Name) -Replace @{extensionAttribute10=($line.'Co-Dueño')} -Passthru # Attribute in AD.
-        #Set-ADGroup -Identity ($line.Name) -Replace @{edsvaSecondaryOwners=($line.'Co-Dueño')} -Passthru # edsvaSecondaryOwners this attibute is only for ActiveRoles Server.
-        Set-ADGroup -Identity ($line.Name) -Replace @{info=($line.Info)} -Passthru
-        Start-Sleep -s 1
+
+        try 
+        {
+            Set-ADGroup -Identity ($line.Name) -Replace @{info=($line.Info)} -Passthru
+        }
+        catch 
+        {
+           write-host "info is in blank, is not posible update atributo info, ($line.Info)"
+           $Errortry = ($line.Name + ";" + ($line.Description) + ";" + ($line.Managedby) + ";" + ($line.Info) + ";" + $_ + ";Fecha: " + (Get-Date))
+           Add-Content ($ErrorPath) -value ($Errortry)
+
+        }
+
         }
     Catch
         {
         # Catch any error
         Write-Host "Group updated failed, ($line.Name)"
-        $Errortry = ($line.Name + "," + $_ + ",Fecha: " + (Get-Date))
-        Add-Content E:\Scripts\Error.txt -value ($Errortry)
-        #New-ADGroup -Name ($line.Name) -SamAccountName ($line.Name) -GroupCategory Security -GroupScope Global -DisplayName ($line.Name) -Path "OU=Usuarios por Eliminar,DC=pruebas,DC=local" -Description ($line.Description)
+        $Errortry = ($line.Name + ";" + ($line.Description) + ";" + ($line.Managedby) + ";" + ($line.Info) + ";" + $_ + ";Fecha: " + (Get-Date))
+        Add-Content ($ErrorPath) -value ($Errortry)
         }
 }
