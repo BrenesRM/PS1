c9d04d440b1924d15398143827306a881c174262
Marlon Brenes <91922207+BrenesRM@users.noreply.github.com>
2022-09-12 14:34:24 -0600

    Inicial

---
+++ TransformImageBase64.ps1
@@ -0,0 +1,7 @@
+﻿#Transforma la jpg a Base64
+[Convert]::ToBase64String((Get-Content -Path D:\1.jpg -Encoding Byte)) >> capture.txt
+
+#La pasa a jpg
+$Base64 = Get-Content -Raw -Path D:\capture.txt
+$Image = [Drawing.Bitmap]::FromStream([IO.MemoryStream][Convert]::FromBase64String($Base64))
+$Image.Save("D:\capture.jpg")
